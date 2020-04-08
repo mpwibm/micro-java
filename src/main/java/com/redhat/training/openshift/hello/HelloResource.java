0b2016cd94e6338d332be6341a2a899db39a22f1
Michael (Mike) Williams <34947926+mpwibm@users.noreply.github.com>
2020-04-08 13:59:18 -0400

    adding libraries for servlet
    adding libraries for servlet

--- src/main/java/com/redhat/training/openshift/hello/HelloResource.java
+++ src/main/java/com/redhat/training/openshift/hello/HelloResource.java
@@ -22,8 +22,12 @@ import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;  
 import java.io.IOException;
 import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Path("/")
 public class HelloResource {
